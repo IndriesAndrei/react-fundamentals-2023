b5240d5203d24ce9b15d6edbfdb41cf977cade87
IndriesAndrei <indriesandrei@yahoo.co.uk>
2023-06-13 19:46:01 +0300

    Add counter for advice numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
+import Count from './Count';
 
 function App() {
   const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
+
+  // show an advice when the app start
+  useEffect(function() {
+    getAdvice();
+  }, []);
 
   return (
     <>
       <h1>{advice}</h1> 
-      <button className='advice-button' onClick={getAdvice}>Get advice</button>      
+      <button className='advice-button' onClick={getAdvice}>Get advice</button>  
+      <Count count={count} />
     </>
   )
 }
---
+++ src/Count.tsx
@@ -0,0 +1,7 @@
+const Count = ({count}: {count: any}) => {
+    return (
+        <p>You have read <strong>{count}</strong> pieces of advice</p>    
+    )
+}
+
+export default Count;
